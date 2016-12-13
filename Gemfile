3797084bbe0f0e49005bd5ff759afe5037e67ef4
DC <dashboardrl@gmail.com>
2016-12-12 22:56:07 -0500

    install gem devise

--- Gemfile
+++ Gemfile
@@ -24,3 +24,4 @@ end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rails_12factor', group: :production
+gem 'devise'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # gem devise.
+  config.action_mailer.default_url_options = { host: 'yelper1.herokuapp.com' }
+
 end
